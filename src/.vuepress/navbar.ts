6a9f5a0fbdb1a3aadf148a3c471eae1bde05c0e2
81520 <815204388@qq.com>
2024-10-29 21:32:27 +0800

    更新顶部导航栏

--- src/.vuepress/navbar.ts
+++ src/.vuepress/navbar.ts
@@ -2,7 +2,6 @@ import { navbar } from "vuepress-theme-hope";
 
 export default navbar([
   "/",
-  "/portfolio",
   "/demo/",
   {
     text: "指南",
@@ -23,9 +22,9 @@ export default navbar([
       },
     ],
   },
-  {
-    text: "V2 文档",
-    icon: "book",
-    link: "https://theme-hope.vuejs.press/zh/",
-  },
+  // {
+  //   text: "V2 文档",
+  //   icon: "book",
+  //   link: "https://theme-hope.vuejs.press/zh/",
+  // },
 ]);
